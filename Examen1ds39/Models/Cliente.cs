fa371249ae8f9eb189d156cee1fe0af3c9f7be21
AdilsonAr <cycles.250@gmail.com>
2021-03-23 22:55:35 -0600

    modelos

---
+++ Examen1ds39/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen1ds39.Models
+{
+    public class Cliente
+    {
+
+        public int cod_cliente { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string dui { get; set; }
+        public string direccion { get; set; }
+        public string nit { get; set; }
+    }
+}
---
+++ Examen1ds39/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen1ds39.Models
+{
+    public class Usuario
+    {
+        public int cod_user { get; set; }
+        public string nombre_usuario { get; set; }
+        public string contra { get; set; }
+        public string nivel_usuario { get; set; }
+
+    }
+}
